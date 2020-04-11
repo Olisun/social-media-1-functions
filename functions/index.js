6801d3f36e90b5e2a34667b1c8be4abd173572f8
Oliver Sun <olisun@mac.com>
2020-04-10 19:06:01 -0700

    installed firebase functions and updated index.js to allow CORS

---
+++ functions/index.js
@@ -0,0 +1,14 @@
+const cors = require('cors')({ origin: true });
+const functions = require('firebase-functions');
+
+// Create and Deploy Your First Cloud Functions
+// https://firebase.google.com/docs/functions/write-firebase-functions
+
+exports.helloWorld = functions.https.onRequest((request, response) => {
+  cors(request, response, () => {
+    console.log("Hello");
+    response.send("Hello Wiggly");
+  })
+});
+
+
