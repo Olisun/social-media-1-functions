2500b4207ccf77c7b7e3ff09ff0c8a0a6f2538b6
Oliver Sun <olisun@mac.com>
2020-04-11 19:20:58 -0700

    install express. refactored get and post requests so that express takes care of the requests in index.js

--- functions/index.js
+++ functions/index.js
@@ -4,55 +4,58 @@ const admin = require('firebase-admin');
 
 admin.initializeApp();
 
-// Create and Deploy Your First Cloud Functions
-// https://firebase.google.com/docs/functions/write-firebase-functions
+const express = require('express');
+const app = express();
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  cors(request, response, () => {
-    console.log("Hello");
-    response.send("Hello Wiggly");
-  })
+
+app.get('/posts', (request, response) => {
+  // cors(request, response, () => {
+  admin
+    .firestore()
+    .collection('posts')
+    .orderBy('createdAt', 'desc')
+    .get()
+    .then(data => {
+      let posts = [];
+      data.forEach(doc => {
+        posts.push({
+          postId: doc.id,
+          body: doc.data().body,
+          userHandle: doc.data().userHandle,
+          createdAt: doc.data().createdAt
+        });
+      });
+      return response.json(posts);
+    })
+    .catch(error => console.error(error));
+  // });
+})
+
+app.post('/posts', (request, response) => {
+  // cors(request, response, () => {
+  const newPost = {
+    body: request.body.body,
+    userHandle: request.body.userHandle,
+    createdAt: admin.firestore.Timestamp.fromDate(new Date())
+  };
+  admin
+    .firestore()
+    .collection('posts')
+    .add(newPost)
+    .then(doc => {
+      response.json({
+        message: `document ${doc.id} created successfully`
+      });
+      return response.json(newPost);
+    })
+    .catch(error => {
+      response.status(500).json({
+        error: 'Oops - something went wrong!'
+      });
+      console.log(error);
+    })
+  // });
 });
 
-exports.getPosts = functions.https.onRequest((request, response) => {
-  cors(request, response, () => {
-    admin
-      .firestore()
-      .collection('posts')
-      .get()
-      .then(data => {
-        let posts = [];
-        data.forEach(doc => {
-          posts.push(doc.data());
-        });
-        return response.json(posts);
-      })
-      .catch(error => console.error(error));
-  });
-});
-
-exports.createPosts = functions.https.onRequest((request, response) => {
-  cors(request, response, () => {
-    const newPost = {
-      body: request.body.body,
-      userHandle: request.body.userHandle,
-      createdAt: admin.firestore.Timestamp.fromDate(new Date())
-    };
-    admin
-      .firestore()
-      .collection('posts')
-      .add(newPost)
-      .then(doc => {
-        response.json({
-          message: `document ${doc.id} created successfully`
-        });
-        return response.json(newPost);
-      })
-      .catch(error => {
-        response.status(500).json({
-          error: 'Oops - something went wrong!'
-        });
-        console.log(error);
-      })
-  });
-});
+// passing the app into the function so it turns into multiple routes.
+exports.api = functions.https.onRequest(app);
