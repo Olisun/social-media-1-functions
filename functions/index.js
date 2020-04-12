a7283885655cb37e13855b8c4a7f759ad0169c25
Oliver Sun <olisun@mac.com>
2020-04-12 12:41:54 -0700

    added firebase authentication in index.js

--- functions/index.js
+++ functions/index.js
@@ -4,10 +4,26 @@ const admin = require('firebase-admin');
 
 admin.initializeApp();
 
-const express = require('express');
-const app = express();
+// initializing express and calling it on the same line..
+const app = require('express')();
 
+// firebase config for authentication.
+const firebaseConfig = {
+  apiKey: "AIzaSyD1eLFpyIA55kILq-Gieac0wFYe16f366A",
+  authDomain: "social-media-1-8a263.firebaseapp.com",
+  databaseURL: "https://social-media-1-8a263.firebaseio.com",
+  projectId: "social-media-1-8a263",
+  storageBucket: "social-media-1-8a263.appspot.com",
+  messagingSenderId: "665970684711",
+  appId: "1:665970684711:web:e207a3f2f48e03f6112f02",
+  measurementId: "G-EH6CH793SJ"
+};
 
+// initializing auth library by passing in the firebase config.
+const firebase = require('firebase');
+firebase.initializeApp(firebaseConfig);
+
+// route for getting all posts.
 app.get('/posts', (request, response) => {
   // cors(request, response, () => {
   admin
@@ -31,6 +47,7 @@ app.get('/posts', (request, response) => {
   // });
 })
 
+// route for submitting a post.
 app.post('/posts', (request, response) => {
   // cors(request, response, () => {
   const newPost = {
@@ -55,6 +72,30 @@ app.post('/posts', (request, response) => {
       console.log(error);
     })
   // });
+});
+
+// route for signing up. 
+app.post('/signup', (request, response) => {
+  // extracting data from the signup form. 
+  const newUser = {
+    email: request.body.email,
+    password: request.body.password,
+    confirmPassword: request.body.confirmPassword,
+    handle: request.body.handle,
+  }
+  // TODO: validate user data
+  firebase.auth().createUserWithEmailAndPassword(newUser.email, newUser.password)
+    .then(data => {
+      return response.status(201).json({
+        message: `user ${data.user.uid} sdignerd up successfully`
+      });
+    })
+    .catch(error => {
+      console.error(error);
+      return response.status(500).json({
+        error: error.code
+      });
+    });
 });
 
 // passing the app into the function so it turns into multiple routes.
